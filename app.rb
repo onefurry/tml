da53c9f2f972decb3d95dafc7b246828f7c13c5c
soaproap <soaproap@gmail.com>
2016-07-18 13:23:39 -0400

    REDIS experimentation

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require 'sinatra'
+require 'redis'
+
+# Use localhost if no redis url set.
+if ENV['REDIS_URL'] == nil
+  ENV['REDIS_URL'] = 'redis://localhost'
+end
+
+# Uses env["REDIS_URL"]
+$r = Redis.new
+
+# Set up any uninitialized redis values.
+$r.setnx('tml:counterfun', '0')
 
 helpers do
   # Returns the number of saved TML games in our registry.
   def n_games
-    0
+    $r.get('tml:counterfun')
   end
 end
 
@@ -13,4 +25,9 @@ end
 
 get '/editor' do
   erb :nyi
+end
+
+get '/increase' do
+  $r.incr('tml:counterfun')
+  redirect to '/'
 end
